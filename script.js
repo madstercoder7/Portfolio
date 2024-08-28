43597c65b414a24ed5a32a55fc9b137ad8d91460
Mohit Dewani <mohitcap177@gmail.com>
2024-08-28 22:13:24 +0530

    Fixed issues with loading screen and started working on the about me page

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ window.addEventListener('load', () => {
     const loadingScreen = document.querySelector('.loading-screen');
     const mainContent = document.querySelector('.main-content');
 
+    mainContent.style.opacity = '1';
+
     setTimeout(() => {
         loadingScreen.style.opacity = '0';
         setTimeout(() => {
             loadingScreen.style.display = 'none';
-            mainContent.style.display = 'block';
             document.body.style.overflow = 'auto';
         }, 500);
     }, 2000);
